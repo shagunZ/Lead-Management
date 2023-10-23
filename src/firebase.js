d5789438b24e7cc643b758b3974b2fe48bec64ae
Shagun Mengi <shagunmengi003@gmail.com>
2023-10-23 13:48:48 +0530

    firebase-setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbOfS69rwxEz9AASnh_PQyHlHcZRcs0-E",
+  authDomain: "lead-management-36cec.firebaseapp.com",
+  projectId: "lead-management-36cec",
+  storageBucket: "lead-management-36cec.appspot.com",
+  messagingSenderId: "854412744116",
+  appId: "1:854412744116:web:641300375a3e79285f4ee1",
+  measurementId: "G-J10H48DSZP"
+};
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { app, auth };
