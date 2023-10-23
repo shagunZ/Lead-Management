5a188d6dce033e92b9379fd7bb76f445bcc0942d
Shagun Mengi <shagunmengi003@gmail.com>
2023-10-23 21:34:46 +0530

    gmail

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import emailjs from "emailjs-com";
+import emailjs from 'emailjs-com';
 
 import InputControl from "../InputControl/InputControl";
 import { auth } from "../../firebase";
@@ -9,27 +9,34 @@ import { auth } from "../../firebase";
 import styles from "./Signup.module.css";
 
 function Signup() {
-  const [emailjsData, setEmailjsData] = useState({
-    user_name: "",
-    user_email: "",
-    message: "",
-  });
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+const form = useRef();
+//
 
-    emailjs
-      .sendForm("gmail", "template_xckno4s", emailjsData, "1-Y5d5TGmZp1y_gYP")
-      .then(
-        (result) => {
-          console.log(result.text);
-          e.target.reset();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  // const sendEmail = (emailjsData) => {
+  // console.log("dddd")
+  //   // Send Email to User's Email Address
+  //   emailjs.sendForm('gmail', 'template_xckno4s', emailjsData, '1-Y5d5TGmZp1y_gYP')
+  //     .then((result) => {
+  //       console.log("aaa")
+  //         console.log(result.text);
+  //        console.log("bbb")
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+  // };
+
+
+  const sendEmail = async (emailjsData) => {
+    try {
+      const result = await emailjs.send('service_219robq', 'template_xckno4s', emailjsData, '1-Y5d5TGmZp1y_gYP');
+      console.log(result.text);
+    } catch (error) {
+      console.error(error);
+    }
   };
+  
+  //
 
   const navigate = useNavigate();
   const [values, setValues] = useState({
@@ -37,16 +44,18 @@ function Signup() {
     email: "",
     pass: "",
   });
-
   const [errorMsg, setErrorMsg] = useState("");
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
 
-  const handleSubmission = () => {
+  const handleSubmission = (e) => {
+    console.log("enter")
+    e.preventDefault();
     if (!values.name || !values.email || !values.pass) {
       setErrorMsg("Fill all fields");
       return;
     }
     setErrorMsg("");
+    console.log("a enter")
 
     setSubmitButtonDisabled(true);
     createUserWithEmailAndPassword(auth, values.email, values.pass)
@@ -56,6 +65,16 @@ function Signup() {
         await updateProfile(user, {
           displayName: values.name,
         });
+    console.log("f enter")
+    const emailjsData = {
+      user_name: values.name,
+      user_email: values.email,
+      user_password: values.pass,
+    };
+    console.log("d enter")
+        sendEmail(emailjsData);
+        console.log("d2 enter")
+
         navigate("/Dashboard");
       })
       .catch((err) => {
@@ -67,47 +86,47 @@ function Signup() {
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
-        <form onSubmit={sendEmail}>
-          <h1 className={styles.heading}>Signup</h1>
+{/* <form onSubmit={handleSubmission}> */}
+        <h1 className={styles.heading}>Signup</h1>
 
-          <InputControl
-            label="Name"
-            placeholder="Enter your name"
-            value={values.name}
-            onChange={(event) =>
-              setValues((prev) => ({ ...prev, name: event.target.value }))
-            }
-          />
-          <InputControl
-            label="Email"
-            placeholder="Enter email address"
-            value={values.email}
-            onChange={(event) =>
-              setValues((prev) => ({ ...prev, email: event.target.value }))
-            }
-          />
-          <InputControl
-            label="Message"
-            placeholder="Enter message"
-            value={emailjsData.message}
-            onChange={(event) =>
-              setEmailjsData({ ...emailjsData, message: event.target.value })
-            }
-          />
+        <InputControl
+        name="user_name"
+          label="Name"
+          placeholder="Enter your name"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
+        />
+        <InputControl
+        name="user_email"
+          label="Email"
+          placeholder="Enter email address"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+        <InputControl
+        name="user_password"
+          label="Password"
+          placeholder="Enter password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+        />
 
-          <div className={styles.footer}>
-            <b className={styles.error}>{errorMsg}</b>
-            <button onClick={handleSubmission} disabled={submitButtonDisabled}>
-              Signup
-            </button>
-            <p>
-              Already have an account?{" "}
-              <span>
-                <Link to="/login">Login</Link>
-              </span>
-            </p>
-          </div>
-        </form>
+        <div className={styles.footer}>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={handleSubmission} disabled={submitButtonDisabled}>
+            Signup
+          </button>
+          <p>
+            Already have an account?{" "}
+            <span>
+              <Link to="/login">Login</Link>
+            </span>
+          </p>
+        </div>
+        {/* </form> */}
       </div>
     </div>
   );
